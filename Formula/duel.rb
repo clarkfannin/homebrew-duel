3382d7da0ee98a7d6eb70a09c2dec85fd6b0a8a2
size12husky <jclarkfii@gmail.com>
2025-12-11 20:45:51 -0600

    Update to v0.2.0

--- Formula/duel.rb
+++ Formula/duel.rb
@@ -1,8 +1,8 @@
 class Duel < Formula
   desc "CLI 1v1 dueling game"
   homepage "https://github.com/clarkfannin/cli-duel"
-  url "https://github.com/clarkfannin/cli-duel/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "859520e0ac22df88da6ad038c7342222f532e81c097f104fbd9fcbeceb66bd3d"
+  url "https://github.com/clarkfannin/cli-duel/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "7c4b5e32bef78b38cf37817f867634bb0c8bbd6354eb1b032e0df806f75800b7"
   license "MIT"
 
   depends_on "go" => :build
@@ -12,6 +12,6 @@ class Duel < Formula
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/duel 2>&1", 0)
+    assert_match "duel", shell_output("#{bin}/duel help 2>&1", 0)
   end
 end
